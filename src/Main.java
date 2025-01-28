d66a09b9d3f769e468219cb8bc0899d05050cee9
user <aytac.karimoffa@gmaill.com>
2025-01-28 20:53:01 +0400

    sdfg

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,9 @@ public class Main {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
+        System.out.println("Hello, ;lokhjghnfbf!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
     }
 }
